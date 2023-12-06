5876d0c51e0e4d2ceafb25bd1f14eb5a32985315
travis-mortimer <travis.mortimer@ayahealthcare.com>
2023-12-05 22:10:18 -0700

    day 5 part 2

--- Puzzles/DayFive/Puzzle.cs
+++ Puzzles/DayFive/Puzzle.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using AdventOfCode.Common;
 
 namespace AdventOfCode.DayFive;
@@ -13,8 +14,6 @@ public class Puzzle : PuzzleBase
     /// </summary>
     public override async Task<int> SolvePartOneAsync()
     {
-        // Tuple<Range, Range>[] map;
-        // Tuple<Range, Range> mapping;
         var seeds = Array.Empty<long>();
         var maps = new List<List<Tuple<long, long, long>>>();
         var lines = File.ReadLinesAsync(GetPuzzleInputFilePath);
@@ -50,7 +49,6 @@ public class Puzzle : PuzzleBase
                     var sourceRangeStart = mapping.Item2;
                     var rangeCount = mapping.Item3;
                     if (code >= sourceRangeStart && code <= sourceRangeStart + rangeCount - 1)
-                    // if (Enumerable.Range(sourceRangeStart, rangeCount).Contains(code))
                     {
                         var diff = code - sourceRangeStart;
                         var destRangeStart = mapping.Item1;
@@ -65,11 +63,73 @@ public class Puzzle : PuzzleBase
     }
 
     /// <summary>
-    /// Answer: 
+    /// Answer: 47909639
     /// </summary>
     public override async Task<int> SolvePartTwoAsync()
     {
 
-        return -1;
+        var baseSeeds = new List<long>();
+        var maps = new List<List<Tuple<long, long, long>>>();
+        var lines = File.ReadLinesAsync(GetPuzzleInputFilePath);
+        await foreach (var line in lines)
+        {
+            if (line.StartsWith("seeds"))
+            {
+                var a = line.Split(':')[1];
+                var b = a.Trim();
+                var c = b.Split(' ');
+                var d = c.Select(long.Parse);
+                baseSeeds = d.ToList();
+            }
+            else if (string.IsNullOrWhiteSpace(line))
+            {
+                maps.Add(new List<Tuple<long, long, long>>());
+            }
+            else if (char.IsDigit(line[0]))
+            {
+                var thing = maps.Last();
+                var mappingInput = line.Split(' ').Select(long.Parse).ToArray();
+                thing.Add(Tuple.Create(mappingInput[0], mappingInput[1], mappingInput[2]));
+            }
+        }
+        var seeds = new Queue<Tuple<long, long>>();
+        for (int i = 0; i < baseSeeds.Count(); i += 2)
+        {
+            var seedRangeStart = baseSeeds[i];
+            var seedRangeEnd = seedRangeStart + baseSeeds[i + 1];
+            seeds.Enqueue(Tuple.Create(seedRangeStart, seedRangeEnd));
+        }
+
+        foreach (var map in maps)
+        {
+            var news = new Queue<Tuple<long, long>>();
+            while (seeds.Any())
+            {
+                var broke = false;
+                var (s, e) = seeds.Dequeue();
+                foreach (var (a, b, c) in map)
+                {
+                    var os = long.Max(s, b);
+                    var oe = long.Min(e, b + c);
+                    if (os < oe)
+                    {
+                        news.Enqueue(Tuple.Create(os - b + a, oe - b + a));
+                        if (os > s)
+                            seeds.Enqueue(Tuple.Create(s, os));
+                        if (e > oe)
+                            seeds.Enqueue(Tuple.Create(oe, e));
+
+                        broke = true;
+                        break;
+                    }
+                }
+                if (!broke)
+                    news.Enqueue(Tuple.Create(s, e));
+            }
+            seeds = news;
+        }
+
+        var min = seeds.Min();
+        return (int)min!.Item1;
     }
 }
